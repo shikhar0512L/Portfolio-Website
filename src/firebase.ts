ec54ad7c6db785039b718bc2b359a40a0628cd94
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2026-09-14 16:22:16 +0530

    Obscure Firebase configuration details
    Updated Firebase configuration to hide sensitive information.

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCrEPMRVv4qkYO5KWvQcgEfKvLUD2Smg3A",
-  authDomain: "shikhar-singh-in.firebaseapp.com",
-  projectId: "shikhar-singh-in",
-  storageBucket: "shikhar-singh-in.firebasestorage.app",
-  messagingSenderId: "702061837182",
-  appId: "1:702061837182:web:151df053ba6d92d96e912f",
-  measurementId: "G-SKVJQPP1D7"
+  apiKey: "#####",
+  authDomain: "#######",
+  projectId: "#########",
+  storageBucket: "#########",
+  messagingSenderId: "######",
+  appId: "###########",
+  measurementId: "###########"
 };
 
 // Initialize Firebase
